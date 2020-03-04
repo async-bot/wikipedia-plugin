9c2b3b3ea2385d0cc7b9adbaad10b0b125b95083
Pieter Hordijk <info@pieterhordijk.com>
2020-03-04 19:38:42 +0300

    Added language search endpoint

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Wikipedia
 
+[![Latest Stable Version](https://poser.pugx.org/async-bot/wikipedia-plugin/v/stable)](https://packagist.org/packages/async-bot/wikipedia-plugin)
+[![Build Status](https://travis-ci.org/async-bot/wikipedia-plugin.svg?branch=master)](https://travis-ci.org/async-bot/wikipedia-plugin)
+[![Coverage Status](https://coveralls.io/repos/github/async-bot/wikipedia-plugin/badge.svg?branch=master)](https://coveralls.io/github/async-bot/wikipedia-plugin?branch=master)
+[![License](https://poser.pugx.org/async-bot/wikipedia-plugin/license)](https://packagist.org/packages/async-bot/wikipedia-plugin)
+
 Wikipedia plugin for the Async Bot framework.
 
 # Requirements
--- src/Plugin.php
+++ src/Plugin.php
@@ -4,8 +4,11 @@ namespace AsyncBot\Plugin\Wikipedia;
 
 use Amp\Promise;
 use AsyncBot\Core\Http\Client;
+use AsyncBot\Plugin\Wikipedia\Collection\Search\LanguageSearchResults as LanguageSearchResultsCollection;
 use AsyncBot\Plugin\Wikipedia\Collection\Search\OpenSearchResults as OpenSearchResultsCollection;
+use AsyncBot\Plugin\Wikipedia\Parser\LanguageSearchJson;
 use AsyncBot\Plugin\Wikipedia\Parser\OpenSearchJson;
+use AsyncBot\Plugin\Wikipedia\Validation\JsonSchema\LanguageSearchResults;
 use AsyncBot\Plugin\Wikipedia\Validation\JsonSchema\OpenSearchResults;
 use function Amp\call;
 
@@ -33,6 +36,25 @@ final class Plugin
             $response = yield $this->httpClient->requestJson($url, new OpenSearchResults());
 
             return (new OpenSearchJson())->parse($response);
+        });
+    }
+
+    /**
+     * @url: https://www.mediawiki.org/wiki/API:Languagesearch
+     * @return Promise<LanguageSearchResultsCollection>
+     */
+    public function languageSearch(string $keywords, int $numberOfAllowedTypos = 1): Promise
+    {
+        return call(function () use ($keywords, $numberOfAllowedTypos) {
+            $url = sprintf(
+                'https://en.wikipedia.org/w/api.php?action=languagesearch&format=json&typos=%d&search=%s',
+                $numberOfAllowedTypos,
+                urlencode($keywords),
+            );
+
+            $response = yield $this->httpClient->requestJson($url, new LanguageSearchResults());
+
+            return (new LanguageSearchJson())->parse($response);
         });
     }
 }
